a2bcec6edae9118fc607b351bc78ecf2637f1343
nvduc2307 <nvduc.yt@gmail.com>
2024-06-14 17:53:29 +0700

    update

---
+++ Tools/ElectricColumnGeneral/ElectricColumnGeneralCmd.cs
@@ -0,0 +1,40 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+using CadDev.Extension.ICommand;
+using CadDev.Tools.ElectricColumnGeneral.viewModels;
+using CadDev.Utils;
+
+namespace CadDev.Tools.ElectricColumnGeneral
+{
+    public class ElectricColumnGeneralCmd : ICadCommand
+    {
+        private ElectricColumnGeneralViewModel _electricColumnGeneralViewModel;
+        [CommandMethod("22")]
+        public void Execute()
+        {
+            try
+            {
+                AC.GetInfomation();
+                using (Transaction ts = AC.Database.TransactionManager.StartTransaction())
+                {
+                    try
+                    {
+                        using (DocumentLock documentLock = AC.DocumentCollection.MdiActiveDocument.LockDocument())
+                        {
+                            _electricColumnGeneralViewModel = new ElectricColumnGeneralViewModel(ts);
+                        }
+                        ts.Commit();
+                    }
+                    catch (System.Exception ex)
+                    {
+                        System.Windows.MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+            catch (System.Exception)
+            {
+            }
+        }
+    }
+}
---
+++ Tools/ElectricColumnGeneral/viewModels/ElectricColumnGeneralViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using CadDev.Utils.Selections;
+using CadDev.Utils;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CadDev.Utils.Lines;
+using CadDev.Utils.Compares;
+
+namespace CadDev.Tools.ElectricColumnGeneral.viewModels
+{
+    public partial class ElectricColumnGeneralViewModel : ObservableObject
+    {
+        public Transaction Ts {  get; }
+        public ElectricColumnGeneralViewModel(Transaction ts)
+        {
+            Ts = ts;
+            var mainFaces = ts.SelectObjs<Line>(AC.Editor);
+            var sideFaces = ts.SelectObjs<Line>(AC.Editor);
+            GetMinLine(sideFaces);
+        }
+        private void GetMinLine(IEnumerable<Line> lines)
+        {
+            try
+            {
+                var lys = lines.OrderBy(x => Math.Min(Math.Round(x.StartPoint.Y), Math.Round(x.EndPoint.Y)))
+                    .GroupBy(x => Math.Min(Math.Round(x.StartPoint.Y), Math.Round(x.EndPoint.Y)))
+                    .Select(x => x.ToList())
+                    .FirstOrDefault();
+                if (lys == null) return;
+                var lxs = lys
+                    .OrderBy(x=> Math.Max(Math.Round(x.StartPoint.X), Math.Round(x.EndPoint.X)))
+                    .GroupBy(x=> Math.Max(Math.Round(x.StartPoint.X), Math.Round(x.EndPoint.X)))
+                    .Select(x=>x.ToList())
+                    .LastOrDefault();
+                if (lxs == null) return;
+                var l = lxs.FirstOrDefault();
+
+                var ls = lines.Where(x=>x.StartPoint.IsSeem(l.StartPoint) || x.StartPoint.IsSeem(l.EndPoint) 
+                || x.EndPoint.IsSeem(l.StartPoint) || x.EndPoint.IsSeem(l.EndPoint));
+
+                foreach (var item in ls)
+                {
+                    item.Highlight();
+                }
+
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
