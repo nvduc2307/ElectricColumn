d9aaf8265b6cd565efeb5a7a73b393791d2b61f0
nvduc2307 <nvduc.yt@gmail.com>
2024-06-17 14:01:53 +0700

    update

--- Tools/ElectricColumnGeneral/viewModels/ElectricColumnGeneralViewModel.cs
+++ Tools/ElectricColumnGeneral/viewModels/ElectricColumnGeneralViewModel.cs
@@ -1,15 +1,14 @@
 ﻿using Autodesk.AutoCAD.DatabaseServices;
-using CadDev.Utils.Selections;
 using CadDev.Utils;
-using CommunityToolkit.Mvvm.ComponentModel;
-using CadDev.Utils.Lines;
 using CadDev.Utils.Compares;
+using CadDev.Utils.Selections;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace CadDev.Tools.ElectricColumnGeneral.viewModels
 {
     public partial class ElectricColumnGeneralViewModel : ObservableObject
     {
-        public Transaction Ts {  get; }
+        public Transaction Ts { get; }
         public ElectricColumnGeneralViewModel(Transaction ts)
         {
             Ts = ts;
@@ -27,14 +26,14 @@ namespace CadDev.Tools.ElectricColumnGeneral.viewModels
                     .FirstOrDefault();
                 if (lys == null) return;
                 var lxs = lys
-                    .OrderBy(x=> Math.Max(Math.Round(x.StartPoint.X), Math.Round(x.EndPoint.X)))
-                    .GroupBy(x=> Math.Max(Math.Round(x.StartPoint.X), Math.Round(x.EndPoint.X)))
-                    .Select(x=>x.ToList())
+                    .OrderBy(x => Math.Max(Math.Round(x.StartPoint.X), Math.Round(x.EndPoint.X)))
+                    .GroupBy(x => Math.Max(Math.Round(x.StartPoint.X), Math.Round(x.EndPoint.X)))
+                    .Select(x => x.ToList())
                     .LastOrDefault();
                 if (lxs == null) return;
                 var l = lxs.FirstOrDefault();
 
-                var ls = lines.Where(x=>x.StartPoint.IsSeem(l.StartPoint) || x.StartPoint.IsSeem(l.EndPoint) 
+                var ls = lines.Where(x => x.StartPoint.IsSeem(l.StartPoint) || x.StartPoint.IsSeem(l.EndPoint)
                 || x.EndPoint.IsSeem(l.StartPoint) || x.EndPoint.IsSeem(l.EndPoint));
 
                 foreach (var item in ls)
