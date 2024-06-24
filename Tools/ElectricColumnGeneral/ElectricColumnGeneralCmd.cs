253ddc5519e30b9ee071616d6437752580c4b79f
nvduc2307 <nvduc.yt@gmail.com>
2024-06-24 18:01:34 +0700

    update

--- Tools/ElectricColumnGeneral/ElectricColumnGeneralCmd.cs
+++ Tools/ElectricColumnGeneral/ElectricColumnGeneralCmd.cs
@@ -3,6 +3,7 @@ using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.Runtime;
 using CadDev.Extension.ICommand;
 using CadDev.Tools.ElectricColumnGeneral.viewModels;
+using CadDev.Tools.ElectricColumnGeneral.views;
 using CadDev.Utils;
 
 namespace CadDev.Tools.ElectricColumnGeneral
@@ -15,6 +16,8 @@ namespace CadDev.Tools.ElectricColumnGeneral
         {
             try
             {
+                var v = new ElectricColumnGeneralRuleInstallView();
+                v.ShowDialog();
                 AC.GetInfomation();
                 using (Transaction ts = AC.Database.TransactionManager.StartTransaction())
                 {
@@ -22,7 +25,7 @@ namespace CadDev.Tools.ElectricColumnGeneral
                     {
                         using (DocumentLock documentLock = AC.DocumentCollection.MdiActiveDocument.LockDocument())
                         {
-                            _electricColumnGeneralViewModel = new ElectricColumnGeneralViewModel(ts);
+                            //_electricColumnGeneralViewModel = new ElectricColumnGeneralViewModel(ts);
                         }
                         ts.Commit();
                     }
