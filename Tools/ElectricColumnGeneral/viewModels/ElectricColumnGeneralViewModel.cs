b6f5f39364067047b18d756b2b42d841691a2d42
nvduc2321 <nvduc.gpt@gmail.com>
2024-06-30 22:56:03 +0700

    update

--- Tools/ElectricColumnGeneral/viewModels/ElectricColumnGeneralViewModel.cs
+++ Tools/ElectricColumnGeneral/viewModels/ElectricColumnGeneralViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using Autodesk.AutoCAD.DatabaseServices;
+using CadDev.MVVM.View;
 using CadDev.Tools.ElectricColumnGeneral.models;
 using CadDev.Tools.ElectricColumnGeneral.services;
+using CadDev.Tools.ElectricColumnGeneral.views;
 using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace CadDev.Tools.ElectricColumnGeneral.viewModels
@@ -8,12 +10,19 @@ namespace CadDev.Tools.ElectricColumnGeneral.viewModels
     public partial class ElectricColumnGeneralViewModel : ObservableObject
     {
         public Transaction Ts { get; }
+        public ElectricColumnGeneralView MainView { get; set; }
         public ElectricColumnService ElectricColumnService { get; }
         public ElectricColumnGeneralViewModel(Transaction ts, ElectricColumnGeneralModel electricColumnGeneralModel)
         {
             Ts = ts;
             ElectricColumnService = new ElectricColumnService(electricColumnGeneralModel);
-            ElectricColumnService.CreateElectricColumn();
+            MainView = new ElectricColumnGeneralView() { DataContext = this};
+            MainView.Loaded += MainView_Loaded;
+            MainView.ShowDialog();
+        }
+
+        private void MainView_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
         }
     }
 }
