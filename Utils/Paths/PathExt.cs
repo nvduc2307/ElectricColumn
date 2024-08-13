f96194ca770eb67e9b9479c195d102ce236c8102
nvduc2307 <nvduc.yt@gmail.com>
2024-08-13 09:19:35 +0700

    update

---
+++ Utils/Paths/PathExt.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+namespace CadDev.Utils.Paths
+{
+    public static class PathExt
+    {
+        public static string AssemblyDirectory
+        {
+            get
+            {
+                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return System.IO.Path.GetDirectoryName(path);
+            }
+        }
+    }
+}
