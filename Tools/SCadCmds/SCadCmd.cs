397e0c182ba02e3f347559eb8aa7b63f415a7a27
nvduc2307 <nvduc.yt@gmail.com>
2024-08-20 11:13:55 +0700

    update

--- Tools/SCadCmds/SCadCmd.cs
+++ Tools/SCadCmds/SCadCmd.cs
@@ -4,6 +4,7 @@ using Autodesk.AutoCAD.Runtime;
 using CadDev.Extension.ICommand;
 using CadDev.Tools.ElectricColumnGeneral.exceptions;
 using CadDev.Utils;
+using CadDev.Utils.CadBlocks;
 using CadDev.Utils.CadTexts;
 using CadDev.Utils.Geometries;
 using CadDev.Utils.Lines;
@@ -16,6 +17,73 @@ namespace CadDev.Tools.SCadCmds
     public class SCadCmd
     {
 
+    }
+
+    public class SCadDMCCmd : ICadCommand
+    {
+        private string _phi = "%%c";
+        private string _markOrigin = "%%c12a200 L=";
+        private string _mark = "xx_%%c12a200 L=_value";
+        private double _spacing = 200;
+        private String _blockTag = "CHU_THICH";
+
+        [CommandMethod("dmlq")]
+        public void Execute()
+        {
+            try
+            {
+                AC.GetInfomation();
+                using (Transaction ts = AC.Database.TransactionManager.StartTransaction())
+                {
+                    try
+                    {
+                        using (DocumentLock documentLock = AC.DocumentCollection.MdiActiveDocument.LockDocument())
+                        {
+
+                            Polyline rPl = null;
+                            Line rl = null;
+                            var objR = ts.PickObject(AC.Editor, "Pick rebar (PolyLine)");
+
+                            Dimension d = null;
+                            var objD = ts.PickObject(AC.Editor, "Pick Dim");
+                            if (objD != null && objD is Dimension) d = objD as Dimension;
+
+                            BlockReference blockRef = null;
+                            var objL1 = ts.PickObject(AC.Editor, "Pick BlockReference");
+                            if (objL1 != null && objL1 is BlockReference) blockRef = objL1 as BlockReference;
+
+
+                            if (objR != null && d != null && blockRef != null)
+                            {
+                                if (objR is Polyline || objR is Line)
+                                {
+                                    //Get rebar length
+                                    var rLength = 0.0;
+                                    if (objR is Polyline pl) rLength = Math.Round(pl.Length, 0);
+                                    if (objR is Line l) rLength = Math.Round(l.Length, 0);
+                                    //Get rebar quantity
+                                    var dL = d.Measurement;
+                                    var du = dL % _spacing;
+                                    var qtyOdd = 1 + (dL - du) / _spacing;
+                                    var qty = du * 100 / _spacing >= 50 ? qtyOdd + 1 : qtyOdd;
+                                    //Change content in block
+                                    var contentChange = $"{qty}{_markOrigin}{rLength}";
+                                    ts.EditBlock(AC.Database, blockRef, _blockTag, contentChange);
+                                }
+                            }
+                            ts.Commit();
+                        }
+                    }
+                    catch (System.Exception ex)
+                    {
+                        if (ex.Message != ObjectNotFoundException.MessageError) IO.ShowWarning("Đối tượng đã chọn không phù hợp");
+                    }
+                }
+            }
+            catch (System.Exception)
+            {
+            }
+        }
     }
 
     public class SCadDMLCmd : ICadCommand
@@ -167,6 +235,41 @@ namespace CadDev.Tools.SCadCmds
                                 var l = lCad3.Create();
                                 l.Layer = l1.Layer;
                             }
+                        }
+                        ts.Commit();
+                    }
+                    catch (System.Exception ex)
+                    {
+                        if (ex.Message != ObjectNotFoundException.MessageError) IO.ShowWarning("Đối tượng đã chọn không phù hợp");
+                    }
+                }
+            }
+            catch (System.Exception)
+            {
+            }
+        }
+    }
+
+    public class SCadBlockCmd : ICadCommand
+    {
+        [CommandMethod("readBlock")]
+        public void Execute()
+        {
+            try
+            {
+                AC.GetInfomation();
+                using (Transaction ts = AC.Database.TransactionManager.StartTransaction())
+                {
+                    try
+                    {
+                        using (DocumentLock documentLock = AC.DocumentCollection.MdiActiveDocument.LockDocument())
+                        {
+                            BlockReference blockRef = null;
+                            var objL1 = ts.PickObject(AC.Editor, "Pick BlockReference");
+                            if (objL1 != null && objL1 is BlockReference) blockRef = objL1 as BlockReference;
+
+                            ts.EditBlock(AC.Database, blockRef, "CHU_THICH", "PHUONG");
+
                         }
                         ts.Commit();
                     }
---
+++ Utils/CadBlocks/CadBlock.cs
@@ -0,0 +1,42 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+
+namespace CadDev.Utils.CadBlocks
+{
+    public class CadBlock
+    {
+    }
+    public static class CadBlockExt
+    {
+        public static void EditBlock(this Transaction ts, Database db, BlockReference blockRef, string tagAttribute = "tagAttribute", string contentChange = "Phu")
+        {
+            try
+            {
+                BlockTable acBlkTbl;
+                acBlkTbl = ts.GetObject(db.BlockTableId, OpenMode.ForWrite) as BlockTable;
+                BlockTableRecord acBlkTblRec;
+                acBlkTblRec = ts.GetObject(acBlkTbl[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+                var properties = blockRef.AttributeCollection;
+                AttributeReference attributeReferenceTarget = null;
+                foreach (ObjectId id in properties)
+                {
+                    var attRef = (AttributeReference)ts.GetObject(id, OpenMode.ForWrite);
+                    if (attRef.Tag == tagAttribute)
+                    {
+                        attributeReferenceTarget = attRef;
+                        break;
+                    }
+                }
+                if (attributeReferenceTarget != null)
+                {
+                    attributeReferenceTarget.TextString = contentChange;
+                }
+                blockRef.SetDatabaseDefaults();
+                acBlkTblRec.AppendEntity(blockRef);
+                ts.AddNewlyCreatedDBObject(blockRef, true);
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
